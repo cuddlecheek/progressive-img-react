bfa4f731b2722ac1c70422c42c73ca05f36845c1
Marek Kadlčík <makadlcik@gmail.com>
2018-09-21 22:45:12 +0200

    init repository

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+
+export default class ProgressiveImg extends PureComponent {
+  render = ({
+    placeholder,
+    src,
+    srcset,
+    sizes,
+    alt,
+    loadStrategy,
+    intersectionMargin
+  }) => {
+    return null;
+  };
+}
